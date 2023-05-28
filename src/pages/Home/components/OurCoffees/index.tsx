c7b6c58000b4d8a0f11bc43f63f83f4fda2aad39
Italo <italodopeey@gmail.com>
2023-05-28 13:52:59 -0300

    inserção de dados

---
+++ src/pages/Home/components/OurCoffees/index.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import CoffeesCard from "../CoffeesCard";
+import { CoffeesList, OurCoffeesContainer } from "./styles";
+import { api } from "../../../../services/api";
+import { Coffees } from "../../../../@types/Coffes";
+
+
+export default function OurCoffees() {
+  const[coffees, setCoffees] = useState<Coffees[]>([])
+
+  useEffect(() => {
+    const loadData = async () => {
+      const { data: coffees } = await api.get('/coffe')
+      setCoffees(coffees[0].contents)
+    }
+
+    loadData()
+
+  }, [])
+
+  console.log(coffees)
+
+  return (
+    <OurCoffeesContainer>
+      <h1>Nossos cafés</h1>
+
+      <CoffeesList>
+        {coffees.map(coffee => (
+          <CoffeesCard key={coffee.id} coffee={coffee} />
+        ))}
+      </CoffeesList>
+    </OurCoffeesContainer>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,45 +1,14 @@
-import { useEffect, useState } from "react";
 import IntroPage from "./components/Intro";
-import { api } from "../../services/api";
+import OurCoffees from "./components/OurCoffees";
 
-interface contents {
-  contents: [
-    coffes
-  ]
-}
-
-
-interface coffes{
-  description: string;
-  name: string;
-  photo: string;
-  price: number;
-  tag: [String];
-  id: string;
-}
 
 
 export default function HomePage() {
-  const[data, setData] = useState<coffes[]>([])
-
-  useEffect(() => {
-    const loadData = async () => {
-      const { data: coffees } = await api.get('/coffe')
-      setData(coffees[0].contents)
-    }
-
-    loadData()
-
-  }, [])
-
-  console.log(data)
-  
-
-
-
   return (
     <main>
       <IntroPage />
+
+      <OurCoffees />
     </main>
   )
 }
