432101588d054b7d72deb9349e92e4e5f48b22d7
Italo <italodopeey@gmail.com>
2023-05-30 00:37:39 -0300

    arrumando as rotas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,25 @@
 import { coffeLogo } from '../../assets/directoryImage'
 import { ShoppingCart, MapPin } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 import { CartShoppingButton, GpsAndCartShopping, HeaderContainer } from './styles'
 
 export default function Header() {
   return (
     <HeaderContainer>
-      <img src={coffeLogo} alt="Coffe Delivery LogoTipo/Apresentação" />
+      <NavLink to={'/'}>
+        <img src={coffeLogo} alt="Coffe Delivery LogoTipo/Apresentação" />
+      </NavLink>
       <GpsAndCartShopping>
         <span>
           <MapPin size={24} weight='fill' />
           Parnamirin RN
         </span>
 
-        <CartShoppingButton type='button'>
-          <ShoppingCart size={24} weight='fill' />
-        </CartShoppingButton>
+        <NavLink to={'/completeOrder'}>
+          <CartShoppingButton type='button'>
+            <ShoppingCart size={24} weight='fill' color={'#C47F17'} />
+          </CartShoppingButton>
+        </NavLink>
       </GpsAndCartShopping>
     </HeaderContainer>
   )
