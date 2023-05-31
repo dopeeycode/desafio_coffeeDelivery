ad709a451ebca0dfdb65b1933794b58caddaf527
Italo <italodopeey@gmail.com>
2023-05-31 09:15:59 -0300

    section completedOrder

--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -1,6 +1,67 @@
+import { AddressContent, CompletedOrder, Container, ContainerMethodPayment, ContentAfterContainer, InputContainer, MethodPaymentButtons, NeighborhoodCityAndUF, NumberAndComplement } from "./styles";
+import { MapPinLine, CurrencyDollar, CreditCard, Bank, Money } from 'phosphor-react'
 
 export default function CompleteOrderPage() {
   return (
-    <div>index</div>
+    <CompletedOrder>
+      <Container>
+        <h3>Complete seu pedido</h3>
+        <ContentAfterContainer>
+          <AddressContent>
+          <MapPinLine size={24} />
+            <div>
+              <span>Endereço de Entrega</span>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </AddressContent>
+          <form>
+            <InputContainer>
+              <input type="number" placeholder="CEP" />
+              <input type="text" placeholder="Rua" />
+
+              <NumberAndComplement>
+                <input type="number" placeholder="Número" />
+                <input type="text" placeholder="Complemento" />
+                <span>Opcional</span>
+              </NumberAndComplement>
+              <NeighborhoodCityAndUF>
+                <input type="text" placeholder="Bairro" />
+                <input type="text" placeholder="Cidade" />
+                <input type="text" placeholder="UF" />
+              </NeighborhoodCityAndUF>
+            </InputContainer>
+
+          </form>
+        </ContentAfterContainer>
+        <div>
+        <ContainerMethodPayment>
+          <div>
+            <CurrencyDollar size={24} />
+            <div>
+              <span>Pagamento</span>
+              <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+            </div>
+          </div>
+          <MethodPaymentButtons>
+            <button type="button">
+              <CreditCard size={20} />
+              CARTÃO DE CREDITO
+            </button>
+            <button type="button">
+              <Bank size={20} />
+              CARTÃO DE DEBITO
+            </button>
+            <button type="button">
+              <Money size={20} />
+              DINHEIRO
+            </button>
+          </MethodPaymentButtons>
+        </ContainerMethodPayment>
+        </div>
+      </Container>
+      <div>
+        teste
+      </div>
+    </CompletedOrder>
   )
 }
---
+++ src/pages/CompleteOrder/styles.ts
@@ -0,0 +1,167 @@
+import styled from "styled-components";
+
+export const CompletedOrder = styled.main`
+
+  margin-top: 11rem;
+  display: flex;
+  justify-content: space-between;
+
+  flex-wrap: wrap;
+  
+`
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: .75rem;
+  width: 640px;
+  background: #fff;
+
+  h3 {
+    margin-bottom: .938rem;
+
+    font-family: ${({ theme }) => theme.fonts.title};
+    font-size: 1.125rem;
+    line-height: 1.438rem;
+  }
+
+
+
+`
+
+export const ContentAfterContainer = styled.div`
+  padding: 2.5rem;
+  border-radius: 6px;
+  background: ${({ theme }) => theme.colors["base-card"]};
+
+`
+
+export const AddressContent = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: .625rem;
+  margin-bottom: 2rem;
+
+  p {
+    font-size: .875rem;
+  }
+
+  span {
+    color: ${({ theme }) => theme.colors["base-subtitle"]};
+  }
+
+  svg {
+      color: ${({ theme }) => theme.colors["brand-yellow-dark"]};
+    }
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: .625rem;
+  input {
+    display: flex;
+    gap: 4px;
+    padding: .75rem;
+    border-radius: 4px;
+    background: 1px solid ${({ theme }) => theme.colors["base-input"]};
+
+    border: 1px solid ${({ theme }) => theme.colors["base-button"]};
+
+    display: inline-block;
+
+    &::placeholder {
+      font-size: .875rem;
+      color: 1px solid ${({ theme }) => theme.colors["base-label"]};
+      line-height: 1.125rem;
+
+    }
+
+    &:first-child{
+      width: 200px;
+    }
+  }
+`
+
+export const NumberAndComplement = styled.div`
+  display: flex;
+  gap: .75rem;
+  position: relative;
+
+  & input:nth-child(2) {
+    flex: 1;
+  }
+
+  span {
+    position: absolute;
+    right: .75rem;
+    font-style: italic;
+    font-size:.75rem;
+    color: ${({ theme }) => theme.colors["base-label"]};
+    bottom: 15px;
+  }
+
+`
+
+export const NeighborhoodCityAndUF = styled.div`
+  display: flex;
+  gap: 4px;
+
+  & input:nth-child(1) {
+    margin-inline-end: .6rem;
+  }
+
+  & input:nth-child(2) {
+    flex: 1;
+  }
+
+  & input:last-child{
+    width: 60px;
+  }
+`
+
+export const ContainerMethodPayment = styled.div`
+  padding: 2.5rem;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  gap: .75rem;
+  background: ${({ theme }) => theme.colors["base-card"]};
+
+  > div {
+    display: flex;
+    align-items: flex-start;
+    gap: .75rem;
+
+    p {
+      font-size: .875rem;
+    }
+  }
+  svg{
+    color: ${({ theme }) => theme.colors["brand-purple"]};
+  }
+`
+
+export const MethodPaymentButtons = styled.div`
+  margin-top: 2rem; 
+  display: flex;
+  align-items: center;
+  gap: .75rem;
+  width: 100%;
+  
+
+  button {
+    padding: 1rem;
+    width: 100%;
+    border: 0;
+    display: flex;
+    align-items: center;
+    gap: .75rem;
+    border-radius: 6px;
+    font-size: .75rem;
+    color: ${({ theme }) => theme.colors["base-text"]};
+    line-height: 1.188rem;
+    background: ${({ theme }) => theme.colors["base-button"]};
+
+  }
+`
